be39bf0b816f2909a48eaa3a4ec2b11d9d8746f9
Samara Gonçalves <samarasilva222220@gmail.com>
2025-09-02 14:35:12 -0300

    idade

---
+++ idade.py
@@ -0,0 +1,13 @@
+nome="Samara"
+sobrenome="Silva"
+idade=20
+ano_nasci=2025-idade
+maior_idade=idade>=18
+altura=1.51
+
+print("nome:",nome)
+print("sobrenome:",sobrenome)
+print("idade:",idade)
+print("ano de nascimento:",ano_nasci)
+print("maior de idade?",maior_idade)
+print("altura:",altura)
